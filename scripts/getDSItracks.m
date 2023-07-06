204a5d9786dcb48fc3b4b5fe435284403711cfa0
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-07-06 11:58:44 -0500

    new helper functions

---
+++ scripts/getDSItracks.m
@@ -0,0 +1,30 @@
+function [Ltracks, Rtracks]=getDSItracks(path)
+
+%   Jordan Bilderbeek July 6 2023
+
+%   Helper function to pull the .trk.gz files based on a given path input.
+%   Will separate them into R and L tracks. If the track is not in a given
+%   hemi, we will add it to the left side (ex: corpus callosum).
+
+
+%% getDSItracks
+
+pattern=fullfile(path, '*.trk.gz'); %Search for .trk.gz
+theFiles=dir(pattern);
+
+Ltracks={};
+Rtracks={};
+
+for ii=1:length(theFiles)
+    baseFileName=theFiles(ii).name;
+    fullFileName=fullfile(path, baseFileName);
+    
+    if contains(baseFileName, '_R') % Sort between L and R
+        Rtracks{end+1}=fullFileName;
+    else
+        Ltracks{end+1}=fullFileName;
+    end
+    
+end
+
+
