04653c7b784ce551d37e1ca3aba310f5509531b1
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-07-10 11:23:37 -0500

    new function

---
+++ scripts/custom_legend.m
@@ -0,0 +1,22 @@
+function custom_legend(trk_file, colors)
+
+%   Jordan Bilderbeek July 10 2023
+
+%   Custom legend plotter that formats the trk_files to get the track name
+%   and pairs it with its respective color. We then create a legend with
+%   the track color and name to overlay on DBS figures. 
+
+%% custom_legend
+
+track_fileparts=regexp(trk_file, '/', 'split'); %split by filesep
+alltracks={};
+
+figure(1); hold on;
+for ii=1:length(trk_file)
+    fileparts=track_fileparts{ii}; %get fileparts for one track
+    alltracks{ii}=regexprep(fileparts{end}, '.trk', ''); %regex the .trk out
+    L(ii)=plot(nan, nan, 'Color', colors{ii}, 'LineWidth', 8); %create nan line
+end
+
+lgd=legend(L(1:ii), alltracks, 'Interpreter', 'none', 'FontSize', 12); %create legend
+title(lgd, 'Tracks Visualized')
