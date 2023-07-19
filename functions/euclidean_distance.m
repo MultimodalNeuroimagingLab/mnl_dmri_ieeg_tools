cd6ce8383d4e439318def9c2f1ed298ab43b25c9
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-07-19 16:00:57 -0500

    updates to dist/angle functions

--- functions/euclidean_distance.m
+++ functions/euclidean_distance.m
@@ -1,4 +1,4 @@
-function fg_fromtrk=euclidean_distance(fg_fromtrk, roi, varargin)
+function fg_fromtrk=euclidean_distance(fg_fromtrk, roi)
 
 %   Jordan Bilderbeek July 18 2023
 
@@ -37,22 +37,6 @@ for ii=1:length(fg_fromtrk) %Calculate distance for all
         [~,M]=size(fg_fromtrk(ii).fibers{jj});
         dist=vecl2norm(bsxfun(@minus, fg_fromtrk(ii).fibers{jj}, roi), 1);  
         fg_fromtrk(ii).distance{jj}=reshape(dist, M, []);
-    end
-end
-
-%% Calculate angle
-if nargin > 2 %can add 'angle' tag in function call to compute the angle as well
-    pnorm=roi/norm(roi); 
-    for ii=1:length(fg_fromtrk) 
-        for jj=1:length(fg_fromtrk(ii).fibers) %a vectorized approach to calculating angle instead of iterating through each 3xN array
-            [~,M]=size(fg_fromtrk(ii).fibers{jj});
-            vdiff=fg_fromtrk(ii).fibers{jj}-roi; % Create vector
-            vdiffnorm=sqrt(sum(vdiff.^2, 1)); %Compute norm of vector
-            vdiffnorm=bsxfun(@rdivide, vdiff, vdiffnorm); %Normalize all vectors
-            dot=sum(bsxfun(@times, pnorm, vdiffnorm), 1);
-            %fg_fromtrk(ii).angle{jj}=reshape(acosd(dot), M, []);
-            fg_fromtrk(ii).angle{jj}=reshape(acos(dot), M, []);
-        end
     end
 end
 
---
+++ functions/strm_angle.m
@@ -0,0 +1,33 @@
+function [fg_fromtrk] = strm_angle(fg_fromtrk,roi, size)
+
+%   Jordan Bilderbeek July 19 2023
+%
+%   Finds streamline points that are within a given window size based on
+%   euclidean distance function. We then fit the principal axes to the
+%   streamline points, and calculate the angle between the principal axes
+%   and a vector based on the electrode contact position (roi) and a
+%   projected point onto the line. 
+
+%% strm_angle
+
+for ii=1:length(fg_fromtrk)
+    for jj=1:length(fg_fromtrk(ii).fibers)
+        ind=find(fg_fromtrk(ii).distance{jj}<size);
+        if isempty(ind)
+            fg_fromtrk(ii).angle{jj}=NaN;
+        else
+            fiberpoints=fg_fromtrk(ii).fibers{jj}(:, ind);
+            PCaxis=linreg3(fiberpoints', 'plot');
+            u=abs(PCaxis(1,:)-PCaxis(2,:));
+            v=abs(projectPoint2Line(roi, PCaxis(1,:), PCaxis(2,:))-roi);
+            fg_fromtrk(ii).angle{jj}=atan2(norm(cross(u,v)),dot(u,v));
+        end
+    end
+    
+end
+
+
+
+
+end
+
