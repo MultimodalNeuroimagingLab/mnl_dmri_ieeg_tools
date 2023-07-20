f82a7a3d94b0faee55944f0a2940b5e756801e8e
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-07-19 20:05:36 -0500

    removed dependencies

--- scripts/script08_calc_statistics.m
+++ scripts/script08_calc_statistics.m
@@ -9,20 +9,100 @@
 %% calc statistics - distance and angle (RIGHT SIDE - el 9:16)
 % this assumes we have the right side fg_fromtrk struct and are using
 % elecmatrix(9:16, :)
+subnum=3;
+[my_subject_labels,bids_path] = dmri_subject_list();
+sub_label = my_subject_labels{subnum};
+dsipath=fullfile(bids_path,'BIDS_subjectsRaw', 'derivatives','dsistudio',['sub-' sub_label]);
+[Ltracks, Rtracks]=getDSItracks(dsipath);
+[Ltracks, Rtracks]=gz_unzip(Ltracks, Rtracks);
+
+%Load DWI file:
+switch subnum
+    case 2
+        dwi_file = fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'qsiprep', ['sub-' sub_label],'ses-compact3T01','dwi',['sub-' sub_label '_ses-compact3T01_acq-diadem_space-T1w_desc-preproc_dwi.nii.gz']);
+    case 4
+        dwi_file = fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'qsiprep', ['sub-' sub_label],'ses-mri01','dwi',['sub-' sub_label '_ses-mri01_acq-diadem_space-T1w_desc-preproc_dwi.nii.gz']);
+    otherwise
+        dwi_file = fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'qsiprep', ['sub-' sub_label],'ses-mri01','dwi',['sub-' sub_label '_ses-mri01_rec-none_run-01_space-T1w_desc-preproc_dwi.nii.gz']);
+end
+
+ni_dwi = niftiRead(dwi_file);
+fg_fromtrk = [];
+
+for ss = 1:length(Rtracks)
+    trk_file = Rtracks{ss};
+    if exist(trk_file, 'file')
+
+        [header,tracks] = trk_read(trk_file);
+        header.vox_to_ras = ni_dwi.qto_xyz;
+        transf_mat = header.vox_to_ras;
+        for ii = 1:3
+            transf_mat(:,ii) = transf_mat(:, ii)./header.voxel_size(ii);
+        end
+
+        trk_name=regexp(trk_file, '/', 'split');
+        fg_fromtrk(ss).name = regexprep(trk_name{end}, '_R.trk', '');
+        fg_fromtrk(ss).fibers = cell(length(tracks),1);
+        for kk = 1:length(tracks)
+            this_strm = transf_mat*[tracks(kk).matrix ones(length(tracks(kk).matrix),1)]';
+            fg_fromtrk(ss).fibers{kk} = this_strm(1:3,:);
+            clear this_strm
+        end
+        clear header tracks
+    else
+        warningMessage = sprintf('Warning: Track file does not exist:\n%s', trk_file);
+    return;
+    end
+end
+
+electrodepositions = fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'qsiprep', ['sub-' sub_label], ['sub-' sub_label '_ses-mri01_space-T1w_desc-qsiprep_electrodes.tsv']);
+elecmatrix=readtable(electrodepositions, 'FileType', 'text', 'Delimiter', '\t');
+elecmatrix=table2array(elecmatrix);
 
 elecmatrix=elecmatrix(9:16, :);
 for ii=1:length(elecmatrix)
     elname=ii+8;
     el(elname).name=['R-Electrode ' num2str(elname)];
-    el(elname).trackstats=euclidean_distance(fg_fromtrk, elecmatrix(ii, :), 'angle');
+    el(elname).trackstats=euclidean_distance(fg_fromtrk, elecmatrix(ii, :));
 end
 
 %% calc statistics - distance and angle (LEFT SIDE - el 1:8)
 
+fg_fromtrk = [];
+
+for ss = 1:length(Ltracks)
+    trk_file = Ltracks{ss};
+    if exist(trk_file, 'file')
+
+        [header,tracks] = trk_read(trk_file);
+        header.vox_to_ras = ni_dwi.qto_xyz;
+        transf_mat = header.vox_to_ras;
+        for ii = 1:3
+            transf_mat(:,ii) = transf_mat(:, ii)./header.voxel_size(ii);
+        end
+
+        trk_name=regexp(trk_file, '/', 'split');
+        fg_fromtrk(ss).name = regexprep(trk_name{end}, '_L.trk', '');
+        fg_fromtrk(ss).fibers = cell(length(tracks),1);
+        for kk = 1:length(tracks)
+            this_strm = transf_mat*[tracks(kk).matrix ones(length(tracks(kk).matrix),1)]';
+            fg_fromtrk(ss).fibers{kk} = this_strm(1:3,:);
+            clear this_strm
+        end
+        clear header tracks
+    else
+        warningMessage = sprintf('Warning: Track file does not exist:\n%s', trk_file);
+    return;
+    end
+end
+
+elecmatrix=readtable(electrodepositions, 'FileType', 'text', 'Delimiter', '\t');
+elecmatrix=table2array(elecmatrix);
+
 elecmatrix=elecmatrix(1:8, :);
 for ii=1:length(elecmatrix)
     el(ii).name=['L-Electrode ' num2str(ii)];
-    el(ii).trackstats=euclidean_distance(fg_fromtrk, elecmatrix(ii, :), 'angle');
+    el(ii).trackstats=euclidean_distance(fg_fromtrk, elecmatrix(ii, :));
 end
 
 %% save statistics
