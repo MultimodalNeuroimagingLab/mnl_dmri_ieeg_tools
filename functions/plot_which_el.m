d2614fda8e8c1ca8bbff93c304696202b9159fe4
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-07-25 10:36:36 -0500

    changes to func

--- functions/plot_which_el.m
+++ functions/plot_which_el.m
@@ -1,8 +1,13 @@
 function [coords, tag]=plot_which_el(elStruct)
 
 %   Jordan Bilderbeek July 21 2023
+%
+%   Function to prompt user input for which electrode based on electrode
+%   name. Will search through the structure based on input and index the
+%   coords out.
+%
 
-
+%% plot_which_el
 
 disp('Which sEEG lead would you like to plot? Your options are:')
 for ii=1:length(elStruct)
@@ -13,11 +18,11 @@ out=input('', "s");
 for ii=1:length(elStruct)
     name=elStruct(ii).name;
     if name==out
-        coords=elStruct(ii).positions;
+        coords=elStruct(ii).positions; %search for input
     end
 end
 
-side=mean(coords(:,1));
+side=mean(coords(:,1)); %determine if we are on L or R side to pull tracks and gifti file
 if side < 0
     tag='L';
 else
