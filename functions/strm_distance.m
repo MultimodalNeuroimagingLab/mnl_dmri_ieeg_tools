02e07dba6fdcde19e306150f641307b5692ac0fb
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-07-31 11:41:28 -0500

    update strm_distance
    added mindist to structure

--- functions/strm_distance.m
+++ functions/strm_distance.m
@@ -33,6 +33,7 @@ for ii=1:length(fg_fromtrk) %Calculate distance for all
         [~,M]=size(fg_fromtrk(ii).fibers{jj});
         dist=vecl2norm(bsxfun(@minus, fg_fromtrk(ii).fibers{jj}, roi), 1);  
         fg_fromtrk(ii).distance{jj}=reshape(dist, M, []);
+        fg_fromtrk(ii).mindist{jj}=min(dist);
     end
 end
 
