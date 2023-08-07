0666fc031c1b4ace7057788e5456da377d873a2f
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-08-07 13:24:40 -0500

    Update script07_segmentation2qsiprep.m

--- scripts/script07_segmentation2qsiprep.m
+++ scripts/script07_segmentation2qsiprep.m
@@ -9,29 +9,28 @@
 %   hippocampus_amygdala_lr_preproc.nii.gz file. 
 
 %% run mri convert script
-
-[my_subject_labels,bids_path] = dmri_subject_list();
-sub_name = my_subject_labels{1}; 
+subnum=5;
+[sub_name,bids_path, ~, ~] = limbic_subject_library(subnum);
 
 resample_type= 'Nearest';
-alignTo=fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'freesurfer', ['sub-' sub_name], ['sub-' sub_name '_ses-mri01_T1w_acpc.nii']);
-segmentFile=fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'freesurfer', ['sub-' sub_name], sub_name, 'mri', 'aparc.a2009s+aseg.mgz');
-outfile=fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'freesurfer', ['sub-' sub_name], sub_name, 'mri', 'hippocampus_amygdala_lr.nii.gz');
+alignTo=fullfile(bids_path,'derivatives', 'freesurfer', ['sub-' sub_name], ['sub-' sub_name '_ses-mri01_T1w_acpc.nii']);
+segmentFile=fullfile(bids_path, 'derivatives', 'freesurfer', ['sub-' sub_name], 'mri', 'aparc.a2009s+aseg.mgz');
+outfile=fullfile(bids_path, 'derivatives', 'freesurfer', ['sub-' sub_name], 'mri', 'hippocampus_amygdala_lr.nii.gz');
 
 str = sprintf('! FREESURFER_HOME=/Applications/freesurfer/7.4.1 && source /Applications/freesurfer/7.4.1/SetUpFreeSurfer.sh && mri_convert  --out_orientation RAS --reslice_like %s -rt %s %s %s', alignTo, resample_type, segmentFile, outfile);
 eval(str)
 %% segmentation2qsiprep
 
-acpcT1 = niftiRead(fullfile(bids_path,'BIDS_subjectsRaw','derivatives','freesurfer',['sub-' sub_name],['sub-' sub_name '_ses-mri01_T1w_acpc.nii']));
+acpcT1 = niftiRead(fullfile(bids_path,'derivatives','freesurfer',['sub-' sub_name],['sub-' sub_name '_ses-mri01_T1w_acpc.nii']));
 
 % qsiprep_T1
-qsiprep_dir = fullfile(bids_path,'BIDS_subjectsRaw','derivatives','qsiprep',['sub-' sub_name]);
+qsiprep_dir = fullfile(bids_path, 'derivatives','qsiprep',['sub-' sub_name]);
 qsiprep_T1 = niftiRead(fullfile(qsiprep_dir,'anat',['sub-' sub_name '_desc-preproc_T1w.nii.gz']));
 acpc2qsiprepXform = dtiRawAlignToT1(acpcT1,qsiprep_T1,[],[], false, 1); 
 
 %% Hippocampus
 
-Hip = niftiRead(fullfile(bids_path,'BIDS_subjectsRaw', 'derivatives', 'freesurfer', ['sub-' sub_name], sub_name, 'mri', 'hippocampus_amygdala_lr.nii.gz'));
+Hip = niftiRead(fullfile(bids_path, 'derivatives', 'freesurfer', ['sub-' sub_name], 'mri', 'hippocampus_amygdala_lr.nii.gz'));
 
 Hip.qto_xyz = acpc2qsiprepXform;
 Hip.qto_ijk = inv(acpc2qsiprepXform);
@@ -41,50 +40,10 @@ Hip.sto_ijk = inv(acpc2qsiprepXform);
 Lh_save_name = regexprep(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'freesurfer', ['sub-' sub_name], sub_name, 'mri', 'hippocampus_amygdala_lr.nii.gz'), '.nii.gz', '_preproc.nii.gz');
 niftiWrite(Hip,Lh_save_name)
 
-%% With ANT - have to find the xform - unknown T1 segmentation
-
-leadDBST1=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'anat_t1.nii'));
-acpc2qsiprepXform = dtiRawAlignToT1(leadDBST1, qsiprep_T1,[],[], false, 1); 
-
-%load dorsal, medial, ventral ANT
-R_AD=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'atlases', 'Morel_medium_5vox', 'rh', 'AD.nii')); 
-R_AD.qto_xyz = acpc2qsiprepXform;
-R_AD.qto_ijk = inv(acpc2qsiprepXform);
-R_AD.sto_xyz = acpc2qsiprepXform;
-R_AD.sto_ijk = inv(acpc2qsiprepXform);
-R_AD_save_name = regexprep(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'atlases', 'Morel_medium_5vox', 'rh', 'AD.nii'), '.nii', '_preproc.nii');
-niftiWrite(R_AD,R_AD_save_name)
-
-
-R_AM=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'atlases', 'Morel_medium_5vox', 'rh', 'AM.nii')); 
-R_AM.qto_xyz = acpc2qsiprepXform;
-R_AM.qto_ijk = inv(acpc2qsiprepXform);
-R_AM.sto_xyz = acpc2qsiprepXform;
-R_AM.sto_ijk = inv(acpc2qsiprepXform);
-
-R_AV=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'atlases', 'Morel_medium_5vox', 'rh', 'AV.nii')); 
-R_AV.qto_xyz = acpc2qsiprepXform;
-R_AV.qto_ijk = inv(acpc2qsiprepXform);
-R_AV.sto_xyz = acpc2qsiprepXform;
-R_AV.sto_ijk = inv(acpc2qsiprepXform);
-
-L_AD=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'atlases', 'Morel_medium_5vox', 'lh', 'AD.nii')); 
-L_AD.qto_xyz = acpc2qsiprepXform;
-L_AD.qto_ijk = inv(acpc2qsiprepXform);
-L_AD.sto_xyz = acpc2qsiprepXform;
-L_AD.sto_ijk = inv(acpc2qsiprepXform);
-
-
-L_AM=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'atlases', 'Morel_medium_5vox', 'lh', 'AM.nii')); 
-L_AM.qto_xyz = acpc2qsiprepXform;
-L_AM.qto_ijk = inv(acpc2qsiprepXform);
-L_AM.sto_xyz = acpc2qsiprepXform;
-L_AM.sto_ijk = inv(acpc2qsiprepXform);
-
-
-L_AV=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbs', ['sub-' sub_name], 'atlases', 'Morel_medium_5vox', 'lh', 'AV.nii')); 
-L_AV.qto_xyz = acpc2qsiprepXform;
-L_AV.qto_ijk = inv(acpc2qsiprepXform);
-L_AV.sto_xyz = acpc2qsiprepXform;
-L_AV.sto_ijk = inv(acpc2qsiprepXform);
+%% With ANT 
+% dtiRawAlignToT1 does not work because the segmentations are a matrix
+% subset (nonpadded). SPM will take this information into account, simply
+% run a SPM coregister estimate with LeadDBST1->qsiprep T1 and add the
+% segmentations as extra images. Default coregister estimate settings
+% appeared to work fine. Check results after. 
 
