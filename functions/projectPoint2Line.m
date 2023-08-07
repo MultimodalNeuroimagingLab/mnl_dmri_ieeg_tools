d980201485d9d538f20957aeacf1afab51babfb7
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-08-07 12:31:45 -0500

    Update projectPoint2Line.m

--- functions/projectPoint2Line.m
+++ functions/projectPoint2Line.m
@@ -1,9 +1,19 @@
 function projpoint=projectPoint2Line(point, lp1, lp2)
 
 %   Jordan Bilderbeek July 6
-
-%   Helper function that takes input as a point, and line points 1/2. Will
-%   project the point onto the line. 
+%
+%   Helper function that takes a point and projects it onto a line. The
+%   intended use is for when we have a PC axis from linreg3 output - we can
+%   then project the electrode contacts onto the PC axis (where lp1 and lp2
+%   are the respective start and end points) to form a straight line.
+%
+%   INPUTS:
+%       a) point - xyz of point to be projected
+%       b) lp1 - xyz of line point 1 (start of pc axis)
+%       c) lp2 - xyz of line point 2 (end of px axis)
+%
+%   OUTPUTS:
+%       a) projpoint - xyz of projectedpoint
 
 %% projectPoint2Line
 
