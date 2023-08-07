cb4095348060c98ed84103a351f028965591c9c5
Jordan Bilderbeek <106217185+JordanBld@users.noreply.github.com>
2023-08-07 09:57:22 -0500

    Update glass_brain_fig.m

--- functions/glass_brain_fig.m
+++ functions/glass_brain_fig.m
@@ -1,12 +1,30 @@
 function glass_brain_fig(subnum)
+%   Jordan Bilderbeek August 1; updated August 7
+%
+%   Creates glass brain rendering figure for one subject. Simply input
+%   subnum and rendering will run. EST 20min/subject on an upsample factor
+%   of 1000 (in render_dbs_lead) call. Ensure pathing is correct for your
+%   subject. 
+%
+%   INPUTS:
+%       a) subnum - numerical value (ex:1) for your subject number in
+%       dmri_subject_list call. subnum is limited by the number of subjects
+%       defined in dmri_subject_list function
 
-color={[0 .0706 .0980], [0 .3725 .4510], [.5804 .8235 .7412], [.9137 .8471 .6510], [0.9333 0.6078 0], [0.7922 0.4039 0.0078], [0.6824 0.1255 0.0706]};
+%% glass_brain_fig
 
+close all;
+
+%Set track colors
+color={'#D00000', '#3185FC', '#FFBA08', '#5D2E8C', '#CBFF8C', '#46237A', '#8FE388', '#FF7B9C', '#1B998B', '#FF9B85'};
+
+%Set paths, get bids path and subject label
 setMyMatlabPaths;
 addpath(genpath(pwd));
 [my_subject_labels,bids_path] = dmri_subject_list();
 sub_label = my_subject_labels{subnum};
 
+%Set DSI path and get L and R tracks, and unzip them if necessary
 dsipath=fullfile(bids_path,'BIDS_subjectsRaw', 'derivatives','dsistudio',['sub-' sub_label]);
 [Ltracks, Rtracks]=getDSItracks(dsipath);
 [Ltracks, Rtracks]=gz_unzip(Ltracks, Rtracks);
@@ -23,67 +41,117 @@ switch subnum
         dwi_file = fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'qsiprep', ['sub-' sub_label],'ses-mri01','dwi',['sub-' sub_label '_ses-mri01_rec-none_run-01_space-T1w_desc-preproc_dwi.nii.gz']);
 end
 
+%Load nifti and create trk struct
 ni_dwi = niftiRead(dwi_file);
 [fg_fromtrk]=create_trkstruct(ni_dwi, Rtracks);
 
 %% Plot right tracks
 tic
-figure(1);
+figure('units','normalized','outerposition',[0 0 1 1]);
 g = gifti(fullfile(bids_path,'BIDS_subjectsRaw', 'derivatives', 'qsiprep', ['sub-' sub_label],'pial_desc-qsiprep.R.surf.gii')); %Will need to do a surface of both sides
 h=ieeg_RenderGifti(g); 
 
 hold on
 for ii=1:length(fg_fromtrk)
-    AFQ_RenderFibers(fg_fromtrk(ii),'numfibers',300,'color',color{ii},'newfig', false);
+    AFQ_RenderFibers(fg_fromtrk(ii),'numfibers',300,'color',color(ii, :),'newfig', false);
 end
 hold off
 disp(['Created track render in ' num2str(toc) ' seconds'])
 
 %% Add ROI
 
+%Load ROIs and render them
 hippocampus=niftiRead(fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'freesurfer', ['sub-' sub_label], sub_label, 'mri', 'hippocampus_amygdala_lr_preproc.nii.gz' ));
-renderROI(hippocampus, -32482); %-32482 is fs tag
+hip=renderROI(hippocampus, color(7, :), -32482); %-32482 is fs tag
+
+r_AV=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbsinqsi', ['sub-' sub_label], 'rAV.nii'));
+av=renderROI(r_AV, color(8, :));
+
+r_AD=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbsinqsi', ['sub-' sub_label], 'rAD.nii'));
+ad=renderROI(r_AD, color(9, :));
+
+r_AM=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbsinqsi', ['sub-' sub_label], 'rAD.nii'));
+am=renderROI(r_AM, color(10, :));
+
 
 %% Adding electrodes
 
+%Load electrode TSV with contact location information
 electrode_tsv=readtable(fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'qsiprep', ['sub-' sub_label], ['sub-' sub_label '_ses-mri01_space-T1w_desc-qsiprep_electrodes.tsv']), 'FileType', 'text', 'Delimiter', '\t');
 elecmatrix = [electrode_tsv.x electrode_tsv.y electrode_tsv.z];
 
 %Medtronic 3387 RC+S (total length 57.1mm - extension 46.6mm). R=.75mm
-electrodes={''};
-render_dbs_lead(elecmatrix(ismember(electrode_tsv.name,electrodes{ii})), .75, 46.6, 0)
+electrodes={'RA1', 'RA2', 'RA3', 'RA4'};
+render_dbs_lead(elecmatrix(ismember(electrode_tsv.label,electrodes), :), .75, 46.6, 0)
+
 %Medtronic 3391 RC+S (total length 57.1mm - extension 32.6mm). R=1.5mm
-electrodes={''};
-render_dbs_lead(elecmatrix(ismember(electrode_tsv.name,electrodes{ii})), 1.5, 32.6, 0) 
-h.FaceAlpha = 0.2;
-loc_view(90,0)
+electrodes={'RH1', 'RH2', 'RH3', 'RH4'};
+render_dbs_lead(elecmatrix(ismember(electrode_tsv.label,electrodes), :), 1.5, 32.6, 0) 
 
-addElectrode(elecmatrix(9:16, :), 'b', 0, .2, 9:16); %add blue rendering
-custom_legend(Ltracks, color, sub_label) %add custom legend
-savename=foo
-saveas(gcf, savename);
+%Add sphere around contacts to illustrate current spread
+electrodes={'RA1', 'RA2', 'RA3', 'RA4', 'RH1', 'RH2', 'RH3', 'RH4' };
+addElectrode(elecmatrix(ismember(electrode_tsv.label,electrodes), :), 'b', 0, .2); %add blue rendering
 
-%% Now for the left side
+%Change the FaceAlpha of all the rois
+hip.FaceAlpha=.5;
+av.FaceAlpha=.5;
+ad.FaceAlpha=.5;
+am.FaceAlpha=.5;
+
+%Add legend
+custom_legend(Rtracks, color, sub_label, 1) 
+
+%Rotate the brain in different views and save the output as a .svg file
+loc_view(90, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg90_notrkrender_R.svg']);
+saveas(gcf, savename)
+
+loc_view(180, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg180_notrkrender_R.svg']);
+saveas(gcf, savename)
+
+loc_view(270, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg270_notrkrender_R.svg']);
+saveas(gcf, savename)
+
+loc_view(0, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg0_notrkrender_R.svg']);
+saveas(gcf, savename)
+
+%% Now for the left side - repeat from above
 close all;
+
+%Create track structure, now for the left side. Dont need to reload dwi
+%image or gather L tracks as already done. 
 [fg_fromtrk]=create_trkstruct(ni_dwi, Ltracks);
 
 %% Plot left tracks
 tic
-figure(1);
+figure('units','normalized','outerposition',[0 0 1 1]);
 g = gifti(fullfile(bids_path,'BIDS_subjectsRaw', 'derivatives', 'qsiprep', ['sub-' sub_label],'pial_desc-qsiprep.L.surf.gii')); %Will need to do a surface of both sides
 h=ieeg_RenderGifti(g); 
 
 hold on
 for ii=1:length(fg_fromtrk)
-    AFQ_RenderFibers(fg_fromtrk(ii),'numfibers',300,'color',color{ii},'newfig', false);
+    AFQ_RenderFibers(fg_fromtrk(ii),'numfibers',300,'color',color(ii, :),'newfig', false);
 end
 hold off
 disp(['Created track render in ' num2str(toc) ' seconds'])
 
 %% Add ROI
 
+%Plot ROI, now for left side
 hippocampus=niftiRead(fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'freesurfer', ['sub-' sub_label], sub_label, 'mri', 'hippocampus_amygdala_lr_preproc.nii.gz' ));
-renderROI(hippocampus, -32482); %-32482 is fs tag
+hip=renderROI_viaseg(hippocampus, color(7, :), -32676); %left fs tag
+
+l_AV=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbsinqsi', ['sub-' sub_label], 'lAV.nii'));
+av=renderROI(l_AV, color(8, :));
+
+l_AD=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbsinqsi', ['sub-' sub_label], 'lAD.nii'));
+ad=renderROI(l_AD, color(9, :));
+
+l_AM=niftiRead(fullfile(bids_path, 'BIDS_subjectsRaw', 'derivatives', 'leaddbsinqsi', ['sub-' sub_label], 'lAD.nii'));
+am=renderROI(l_AM, color(10, :));
 
 %% Adding electrodes
 
@@ -91,18 +159,40 @@ electrode_tsv=readtable(fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'qs
 elecmatrix = [electrode_tsv.x electrode_tsv.y electrode_tsv.z];
 
 %Medtronic 3387 RC+S (total length 57.1mm - extension 46.6mm). R=.75mm
-electrodes={''};
-render_dbs_lead(elecmatrix(ismember(electrode_tsv.name,electrodes{ii})), .75, 46.6, 0)
+electrodes={'LA1', 'LA2', 'LA3', 'LA4'};
+render_dbs_lead(elecmatrix(ismember(electrode_tsv.label,electrodes), :), .75, 46.6, 1)
+
 %Medtronic 3391 RC+S (total length 57.1mm - extension 32.6mm). R=1.5mm
-electrodes={''};
-render_dbs_lead(elecmatrix(ismember(electrode_tsv.name,electrodes{ii})), 1.5, 32.6, 0) 
-h.FaceAlpha = 0.2;
+electrodes={'LH1', 'LH2', 'LH3', 'LH4'};
+render_dbs_lead(elecmatrix(ismember(electrode_tsv.label,electrodes), :), 1.5, 32.6, 1) 
 loc_view(90,0)
 
-addElectrode(elecmatrix(9:16, :), 'b', 0, .2, 9:16); %add blue rendering
-custom_legend(Ltracks, color, sub_label) %add custom legend
-savename=foo
-saveas(gcf, savename);
+electrodes={'LA1', 'LA2', 'LA3', 'LA4', 'LH1', 'LH2', 'LH3', 'LH4' };
+addElectrode(elecmatrix(ismember(electrode_tsv.label,electrodes), :), 'b', 0, .2); %add blue rendering
+
+%Change face alpha of ROI
+hip.FaceAlpha=.5;
+av.FaceAlpha=.5;
+ad.FaceAlpha=.5;
+am.FaceAlpha=.5;
+custom_legend(Ltracks, color, sub_label, 1) %add custom legend
+
+%Rotate and save as .svg file in appropriate location
+loc_view(90, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg90_notrkrender_L.svg']);
+saveas(gcf, savename)
+
+loc_view(180, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg180_notrkrender_L.svg']);
+saveas(gcf, savename)
+
+loc_view(270, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg270_notrkrender_L.svg']);
+saveas(gcf, savename)
+
+loc_view(0, 0)
+savename=fullfile(bids_path,'BIDS_subjectsRaw','derivatives', 'figs', ['sub-' sub_label], ['sub-' sub_label '_glassbrain_allseg0_notrkrender_L.svg']);
+saveas(gcf, savename)
 
 end
 
